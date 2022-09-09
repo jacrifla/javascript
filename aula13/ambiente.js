ed1cd15fb8803ee14f0a6e51f93ae092bac0390f
jacrifla <jaquelini_cris@hotmail.com>
2022-09-09 02:00:05 -0300

    aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,34 @@
+var contador = 1
+/*
+do {
+    console.log(`Passo ${contador}`);
+    contador++
+} while(contador <= 6)
+*/
+
+
+while (contador <= 3) {
+    console.log('Tudo bm?');
+    contador += 3
+}
+
+
+for (var c = 1; c <= 3; c += 2){
+    console.log('Tudo bm?');
+}
+
+/*
+while (contador <= 3) {
+    console.log(`Passo ${contador}`);
+    contador++
+}
+*/
+
+/*
+console.log('Tudo bm?');
+console.log('Tudo bm?');
+console.log('Tudo bm?');
+console.log('Tudo bm?');
+console.log('Tudo bm?');
+console.log('Tudo bm?');
+*/
